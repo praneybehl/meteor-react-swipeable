53010d79374fd3aaee4f854e242f7667084e4428
Praney Behl <praneybehl@gmail.com>
2015-11-06 16:36:25 +1100

    Update package dependencies and match version to source version

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
-  name: 'react-swipeable',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  name: 'praneybehl:react-swipeable',
+  version: '3.0.2',
+  summary: 'React swipable events component packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-swipeable',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('react-swipeable.js');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.export('Dropzone');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('react-swipeable');
-  api.addFiles('react-swipeable-tests.js');
+
+Npm.depends({
+  "react-swipeable": "3.0.2",
+  "exposify": "0.5.0"
 });
